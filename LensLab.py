4c93cb0b4f135c0f92de53b56a15b656f755d174
scott-mer <scott.c.mer@gmail.com>
2019-10-22 23:44:28 -0700

    Added Ray class, renamed classes, altered flow

--- LensLab.py
+++ LensLab.py
@@ -10,32 +10,107 @@ class Application():
         self.type = type
         print("Booting Pyght")
 
-#   The system class defines everything about the optical system as a whole
-#   It contains all of the elements and where they are, information about the
-#   environment(temp/pressure), wavelength, shit like that. It'll have a bunch
-#   of methods for insterting elements, and basically just adjusting all of the
-#   described above
 class System():
     """
-    Description: Defines everything about the optical system as a whole
+    Description: Defines everything about the optical system as a whole, i.e. environment
+
+    Notes:
     """
-    def __init__(Self):
-        #sfvnef
-        def insertElement(self):
-            type = input('What type of element (Relfective or Refractive)')
-            new_element = Element(type)
-            self.Elements.append(new_element)
-        #I actually think I want to make a Ray class from the below stuff
-        def TraceRay(self,x0,y0,z0,xd,yd,zd): #x0,y0,z0 are ray launch coords and xd,yd,zd are components of the unit vector direction of the ray
-            t = symbols('t')
-            #find intersection points for each surface and take the one it hits first
-            # I want to make this fairly non sequential. All rays get traced to each surface. not sure how this is gonna go yet
-            for element in self.Elements:
-                for surface in element.surface:
-                    intersect = self.equation.subs(x,x0+t*xd).subs(y,y0+t*yd).subs(z,z0+t*zd)
-                    pathlength = solve(intersect,t) ###spits out path length from launch location to surface intersection
-                    #More stuff to find the XYZ coordinates of intersection, make sure that location is within the specified aperture etc
-                    # then do refraction/reflection stuff
+    def __init__(self):
+        print("New system created")
+    
+    def addSurf(self):
+        """
+        Description: Adds a new surface into the system
+
+        Notes:
+        """
+        new_surf = Surf()
+        self.Surfs.append(new_surf)
+
+    def addRay(self, wl, h):
+        """
+        Description:Adds a new ray, with inputs of wavelength and field height
+
+        Notes:
+        """
+
+        new_ray = Ray()
+        self.Rays.append(new_ray)
+
+class Ray():
+    """
+    Description: Defines rays through the system
+
+    Notes:
+    """
+    def __init__(self, wl, h):
+        Ray.wl = wl
+        Ray.h = h
+
+    def trace(self,x0,y0,z0,xd,yd,zd): 
+        """
+        Description: Traces ray through system, x0, y0, z0 are ray launch coords and xd, yd, zd are components of the unit vector direction of the ray
+        
+        Notes:
+        """
+        t = symbols('t')
+        #find intersection points for each surface and take the one it hits first
+        # I want to make this fairly non sequential. All rays get traced to each surface. not sure how this is gonna go yet
+        for element in self.Elements:
+            for surface in element.surface:
+                intersect = self.equation.subs(x,x0+t*xd).subs(y,y0+t*yd).subs(z,z0+t*zd)
+                pathlength = solve(intersect,t) ###spits out path length from launch location to surface intersection
+                #More stuff to find the XYZ coordinates of intersection, make sure that location is within the specified aperture etc
+                # then do refraction/reflection stuff
+
+class Surf():
+    """
+    Description: Surfaces in the system
+
+    Notes:
+    """
+    def __init__(self, type):
+        Surf.type = type
+
+    def defineRefr(self):
+        """
+        Description: Define properties of a refractive surface
+
+        Notes:
+        """
+        self.shapetype = input()
+        if self.shapetype == 'Paraxial':
+            self.thickness = 0
+            self.focal_length = input()
+            self.semi_diameter = input()
+            self.material = input()
+            self.equation = self.setupSurf('Paraxial')
+
+        elif self.shapetype == 'Spherical':
+            self.equation = self.setupSurf('Sphereical')
+
+    def setupSurf(self):
+        """
+        Description: Sets up the equation of the surface
+
+        Notes:
+        """
+        if self.shapetype == 'Paraxial':
+
+        elif self.shapetype == 'Flat':
+            A,B,C,D,x,y,z = symbols('A B C D x y z')
+
+            self.equation = Eq(A*x+B*y+C*Z+D)
+
+        elif self.shapetype == 'Spherical':
+            x,y,z,x_s,y_s,z_s,R = symbols('x y z x_s y_s z_s R') #underscore s is the shift
+            self.equation = Eq((x-x_s)**2+(y-y_s)**2+(z-z_s)**2-R**2)
+        else:
+            print('Invalid Surface Shape type')
+            return
+
+
 
 #   Ok. Element class. When you want to insert a new element (called from the System class),
 #   you'll be able to select what type of element you want(lens mirror prism etc), then select material,
@@ -52,42 +127,3 @@ class Element():
 
         if self.type == "Reflective"
             self.surface = Surface('Refl')
-
-#   Surface class is for defining surfaces of your element, again as either reflective or Refractive
-#   It also sets up the shape of the surface in the form of an equation
-#   so surface.equation always contains the symbolic equation to that surface
-class Surface():
-    def __init__(self, type):
-        Surface.type = type
-        if Surface.type  == 'Refl':
-            self.setuprefl()
-        elif Surface.type  == 'Refr':
-            self.setuprefr()
-
-
-    def setuprefr(self):
-        self.shapetype = input()
-        if self.shapetype == 'Paraxial':
-            self.thickness = 0
-            self.focal_length = input()
-            self.semi_diameter = input()
-            self.material = input()
-            self.equation = self.setup_equation('Paraxial')
-
-        elif self.shapetype == 'Spherical':
-
-    def setup_equation(self):
-        #More types of surfaces to be added later
-        if self.shapetype == 'Paraxial':
-
-        elif self.shapetype == 'Flat':
-            A,B,C,D,x,y,z = symbols('A B C D x y z')
-
-            self.equation = Eq(A*x+B*y+C*Z+D)
-
-        elif self.shapetype == 'Spherical':
-            x,y,z,x_s,y_s,z_s,R = symbols('x y z x_s y_s z_s R') #underscore s is the shift
-            self.equation = Eq((x-x_s)**2+(y-y_s)**2+(z-z_s)**2-R**2)
-        else:
-            print('Invalid Surface Shape type')
-            return
